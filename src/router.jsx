37086e45254822e0d57ec8ad9a1b9b13dac67fa1
WyattBensman <wyattbensman5@gmail.com>
2024-01-02 15:12:20 -0500

    Created the router.jsx file, all routes work

---
+++ src/router.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter, Outlet } from "react-router-dom";
+import Home from "./pages/Home";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import App from "./App";
+
+export const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/portfolio", element: <Portfolio /> },
+      { path: "/contact", element: <Contact /> },
+    ],
+  },
+]);
