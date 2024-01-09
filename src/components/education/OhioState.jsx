3cc20c8ca7f3df20af3a73b2412f55c63e6ba390
WyattBensman <wyattbensman5@gmail.com>
2024-01-09 12:04:00 -0500

    Added Ohio State Buckeye Leaf, and UCF Knight behind text

--- src/components/education/OhioState.jsx
+++ src/components/education/OhioState.jsx
@@ -1,11 +1,20 @@
 export default function OhioState() {
   return (
-    <div className="w-[550px] text-center text-white p-12 rounded-lg bg-[#1A1A1A]">
-      <h3 className="text-3xl font-bold">The Ohio State University</h3>
-      <h4 className="text-2xl font-light">Bachelor of Science in Marketing</h4>
-      <p className="font-light text-sm">AUGUST 2019 - MAY 2022</p>
+    <div className="relative w-[550px] text-center text-white p-12 rounded-lg bg-[#1A1A1A] overflow-hidden">
+      <h3 className="text-3xl font-bold relative z-10">
+        The Ohio State University
+      </h3>
+      <h4 className="text-2xl font-light relative z-10">
+        Bachelor of Science in Marketing
+      </h4>
+      <p className="font-light text-sm relative z-10">AUGUST 2019 - MAY 2022</p>
+      <div className="absolute inset-0 flex items-center justify-center">
+        <img
+          src="/images/school-logos/ohio-state-buckeye-leaf.png"
+          className="w-2/3 h-auto"
+          alt="Ohio State University Logo"
+        />
+      </div>
     </div>
   );
 }
-
-<img src="/images/school-logos/ohio-state-buckeye-leaf.png" />;
--- src/components/education/UCF.jsx
+++ src/components/education/UCF.jsx
@@ -1,11 +1,22 @@
 export default function UCF() {
   return (
-    <div className="w-[550px] text-center text-white p-12 rounded-lg bg-[#1A1A1A]">
-      <h3 className="text-3xl font-bold">University of Central Florida</h3>
-      <h4 className="text-2xl font-light">
+    <div className="relative w-[550px] text-center text-white p-12 rounded-lg bg-[#1A1A1A] overflow-hidden">
+      <h3 className="text-3xl font-bold relative z-10">
+        University of Central Florida
+      </h3>
+      <h4 className="text-2xl font-light relative z-10">
         Full Stack Web Development Certificate
       </h4>
-      <p className="font-light text-sm">JUNE 2023 - SEPTEMBER 2023</p>
+      <p className="font-light text-sm relative z-10">
+        JUNE 2023 - SEPTEMBER 2023
+      </p>
+      <div className="absolute inset-0 flex items-center justify-center">
+        <img
+          src="/images/school-logos/ucf-knight.png"
+          className="w-2/3 h-auto"
+          alt="Ohio State University Logo"
+        />
+      </div>
     </div>
   );
 }
